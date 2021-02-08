95f286873634d4d960b10adc2671cef9c9c392cc
kiddjsh <70739345+kiddjsh@users.noreply.github.com>
2021-02-08 16:50:49 -0500

    Delete win32game/win32game directory

--- win32game/win32game/utilitymacro.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// isolate each color part, divide by 2, add back in
-#define DARKEN(color) ( \
-	/*red*/   ( (  color        & 0xff ) / 2)        | \
-	/*green*/ ((( (color >> 8 ) & 0xff ) / 2) << 8 ) | \
-	/*blue*/  ((( (color >> 16) & 0xff ) / 2) << 16)   \
-)
